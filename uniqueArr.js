35088b06f784d5d582a073fb5ec837bd19278e88
Medwid-Oleg <medwidoleg@gmal.com>
2024-01-08 11:54:09 +0200

    name change

--- uniqueArr.js
+++ uniqueArr.js
@@ -1,7 +1,7 @@
 let array = [1, 1, 2, 3, 4, 2, 5]
 function arrClear(arr) {
-	return arr.filter((item, index, samMasyv) => {
-		return samMasyv.indexOf(item) === index
+	return arr.filter((item, index, arr) => {
+		return arr.indexOf(item) === index
 	})
 }
 console.log(arrClear(array))
